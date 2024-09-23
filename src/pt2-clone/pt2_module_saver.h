39e7cc67dc41b9b9714324ebd610faec7cac1b82
pepijn-devries <pepijn.devries@wur.nl>
2024-09-23 23:06:02 +0200

    More fixes for the compiler

--- src/pt2-clone/pt2_module_saver.h
+++ src/pt2-clone/pt2_module_saver.h
@@ -7,9 +7,6 @@
 extern "C" {
 #endif
 
-// bool saveModule(bool checkIfFileExist, bool giveNewFreeFilename);
-
-bool modSave(char *fileName); // used by saveModule() and crash handler
 uint32_t modSave2(module_t * my_song, uint8_t *target, uint32_t target_size);
 
 #ifdef __cplusplus
