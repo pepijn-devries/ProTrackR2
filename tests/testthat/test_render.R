13536bceb487a2250ca692695431d290571f3923
pepijn-devries <pepijn.devries@wur.nl>
2024-12-08 00:42:07 +0100

    fix snapshot test

--- tests/testthat/test_render.R
+++ tests/testthat/test_render.R
@@ -1,9 +1,11 @@
 test_that("Module renders as expected", {
-  snap <- "intro.rdata"
+  snap <- "intro.gz"
   announce_snapshot_file(snap)
   mod <- pt2_read_mod(pt2_demo())
-  temprda <- tempfile(fileext = ".Rdata")
+  tempgz <- tempfile(fileext = ".gz")
   rndr <- pt2_render(mod, 0.5)
-  save(rndr, file = temprda, compress = TRUE)
-  expect_snapshot_file(temprda, snap)
+  con <- gzfile(tempgz, "wb", compression = 9)
+  writeBin(c(rndr), con, endian = "little")
+  close(con)
+  expect_snapshot_file(tempgz, snap)
 })
