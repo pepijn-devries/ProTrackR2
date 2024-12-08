ecb489adbcb3dd74aae946ff52e81ed5fda9948e
pepijn-devries <pepijn.devries@wur.nl>
2024-12-08 14:01:05 +0100

    Update vignette. Tolerate some differences in rendered files on different systems

--- tests/testthat/test_render.R
+++ tests/testthat/test_render.R
@@ -8,7 +8,13 @@ test_that("Module renders as expected", {
   audio::save.wave(rndr, tempwav)
   av::av_audio_convert(tempwav, tempmp3, verbose = FALSE)
   mp3_compare <- function(old, new) {
-    identical(av::read_audio_bin(old), av::read_audio_bin(new))
+    bn <- av::read_audio_bin(new)
+    bo <- av::read_audio_bin(old)
+    result <- table(factor(abs((c(bn) - c(bo))/2^31) > 0.025, c("TRUE", "FALSE")))
+    ## mp3 compression is not identical on all operating systems.
+    ## Therefore, tolerate some difference.
+    ## Less than 2.5% of the samples deviates more than 2.5%
+    return (result[[1]]/sum(result, na.rm = TRUE) < 0.25)
   }
   expect_snapshot_file(tempmp3, snap, compare = mp3_compare)
 })
