c2ecc6cbac4dec30bdc16df2e9dccff2f6479fd5
pepijn-devries <pepijn.devries@wur.nl>
2024-12-08 09:22:48 +0100

    use ogg instead of mp3. latter does not reproduce well

--- tests/testthat/test_render.R
+++ tests/testthat/test_render.R
@@ -1,11 +1,14 @@
 test_that("Module renders as expected", {
-  snap <- "intro.mp3"
+  snap <- "intro.ogg"
   announce_snapshot_file(snap)
   mod <- pt2_read_mod(pt2_demo())
   tempwav <- tempfile(fileext = ".wav")
-  tempmp3 <- tempfile(fileext = ".mp3")
+  tempogg <- tempfile(fileext = ".ogg")
   rndr <- pt2_render(mod, 10)
   audio::save.wave(rndr, tempwav)
-  av::av_audio_convert(tempwav, tempmp3, verbose = FALSE)
-  expect_snapshot_file(tempmp3, snap)
+  av::av_audio_convert(tempwav, tempogg, verbose = FALSE)
+  ogg_compare <- function(old, new) {
+    identical(av::read_audio_bin(old), av::read_audio_bin(new))
+  }
+  expect_snapshot_file(tempogg, snap, compare = ogg_compare)
 })
